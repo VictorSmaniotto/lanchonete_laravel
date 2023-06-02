19ac9afd3b3aece3c7f7f5c854b76ac7230cc161
Victor Smaniotto <victorsmaniotto@hotmail.com>
2023-06-02 20:57:37 -0300

    Atualizando as colunas foto e perfil do usuário

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('imagem')->nullable();
+            $table->string('perfil', 20)->default('cliente'); //cliente, administrador
             $table->rememberToken();
             $table->timestamps();
         });
