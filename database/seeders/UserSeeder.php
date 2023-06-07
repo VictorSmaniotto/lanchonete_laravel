c34a14c33d6b356755cc1851dda56039e0ad3254
Victor Smaniotto <victorsmaniotto@hotmail.com>
2023-06-07 19:51:19 -0300

    seeders de usuários

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Faker\Factory as Faker;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create('pt_BR');
+
+        foreach (range(1, 10) as $index) {
+            User::create([
+                'nome' => $faker->name,
+                'email' => $faker->unique()->safeEmail(),
+                'password' => bcrypt('12345678'),
+                'remember_token' => Str::random(10)
+            ]);
+        }
+    }
+}
